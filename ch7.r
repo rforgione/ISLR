193ded643a9d89159572dfe8d6b7f85494f1f02d
Rob Forgione <rforgione@gmail.com>
2016-03-31 04:45:39 +0000

    finish chapter 7!

--- ch7.r
+++ ch7.r
@@ -151,6 +151,7 @@ for(i in 1:10) {
     scores <- c(scores, sum(fit$residuals^2))
 }
 
+scores
 plot(1:10, scores, type="l", lwd=2)
 
 # c. 
@@ -164,3 +165,205 @@ for(i in 1:10) {
 best.degree <- which(cv.scores == cv.scores[cv.scores < min(cv.scores) + sd(cv.scores)*.2][1])
 best.fit <- glm(nox~poly(dis,best.degree), data=Boston)
 
+# d.
+library(splines)
+fit <- glm(nox~bs(dis,4), data=Boston)
+summary(fit)
+
+# Chose the knots by allowing the software to pick them uniformly
+# across the data.
+
+dis.range <- range(Boston$dis)
+dis.grid <- seq(dis.range[1], dis.range[2])
+nox.predicted <- predict(fit, newdata=data.frame(dis=dis.grid))
+plot(nox~dis, data=Boston, col="darkgrey", pch=20)
+lines(dis.grid, nox.predicted, type="l", col="red", lwd=3)
+
+# e.
+rss <- c()
+
+for(i in 1:10) {
+    fit <- glm(nox~bs(dis,df=i), data=Boston)
+    rss <- c(rss, sum(fit$residuals^2))
+}
+
+plot(1:10, rss, type="l", col="darkgrey", lwd=3)
+
+# RSS drops of drastically at df=5 and then slowly
+# continues to drop off after that.
+
+# f.
+cv.scores <- c()
+for(i in 1:10) {
+    fit <- glm(nox~bs(dis,df=i), data=Boston)
+    cv.scores <- c(cv.scores, cv.glm(Boston, fit, K=10)$delta[2])
+}
+
+reasonable <- cv.scores[cv.scores < min(cv.scores) + .2*sd(cv.scores)][1]
+
+degree <- which(cv.scores == reasonable)
+optimal.score <- cv.scores[degree]
+
+# 5 df gives us a reasonably good CV score while also keeping
+# the model relatively simple. Note: a cubic spline with
+# 5 df has one knot -- the 5 degrees of freedom are intercept,
+# X, X^2, X3, K. Unless you specify a different degree, the bs
+# function fits cubic splines.
+
+best.model <- glm(nox~bs(dis,df=degree), data=Boston)
+
+# 10.
+# a.
+n <- nrow(College)
+
+train.pct <- .75
+test.pct <- 1 - train.pct
+
+test.samples <- sample(n,round(test.pct*n),replace=FALSE)
+
+train.set <- College[-test.samples,]
+test.set <- College[test.samples,]
+
+regfit.fwd <- regsubsets(Outstate~.,data=College, method="forward", nvmax=18)
+
+test.mat <- model.matrix(Outstate~.,data=test.set)
+
+errors <- c()
+
+for (i in 1:17) {
+    coefi <- coef(regfit.fwd, id=i)
+    pred <- test.mat[,names(coefi)]%*%coefi
+    errors <- c(errors,mean((test.set$Outstate - pred)^2))
+}
+
+plot(1:17, errors, type="l", col="darkgrey", lwd=3)
+
+reasonable.error <- errors[errors < min(errors) + .2*sd(errors)][1]
+
+reasonable.model <- which(errors == reasonable.error)
+
+reasonable.model
+
+included.vars <- names(coef(regfit.fwd,id=reasonable.model))[-1]
+
+# We choose the model with 8 variables because it is a pretty combination
+# trade-off of simplicity and explanation of the variance. 
+
+# b.
+
+gam.fit <- gam(Outstate~ns(Room.Board, df=4)+ns(Personal,df=4)+ns(PhD, df=4)+ns(Terminal, df=4)+ns(perc.alumni, df=4)+ns(Expend, df=4)+ns(Grad.Rate, df=4)+Private, data=train.set)
+
+par(mfrow=c(4,4))
+plot(gam.fit)
+
+
+# c. 
+
+test.predictions <- predict(gam.fit, newdata=test.set)
+
+mse <- mean((test.predictions - test.set$Outstate)^2)
+
+gam.tss = mean((test.set$Outstate - mean(test.set$Outstate))^2)
+
+r_2 = 1 - mse/gam.tss
+
+# d.
+summary(gam.fit)
+
+# Yes, there appears to be strong evidence of non-linear relationships for every
+# variable except Terminal.
+
+# 11.
+# a.
+set.seed(1)
+g = data.frame(y=rnorm(100), x1=rnorm(100), x2=rnorm(100))
+
+
+# b.
+beta1 <- 2
+
+# c.
+a <- g$y-(beta1*g$x1)
+beta2 <- lm(a~g$x2)$coef[2]
+
+# d.
+a <- g$y-(beta2*g$x2)
+beta1 <- lm(a~g$x1)$coef[2]
+
+# e.
+coefs <- data.frame()
+for(i in 1:10) {
+    if (i == 1) {
+        beta1 <- 100
+    }
+    a <- g$y-(beta1*g$x1)
+    beta2 <- lm(a~g$x2)$coef[2]
+    b <- g$y-(beta2*g$x2)
+    beta1 <- lm(b~g$x1)$coef[2]
+    beta0 <- lm(b~g$x1)$coef[1]
+    
+    print(sprintf("Iteration %s", i))
+    print(sprintf("beta0 is %s", beta0))
+    print(sprintf("beta1 is %s", beta1))
+    print(sprintf("beta2 is %s", beta2))
+    coefs <- rbind(coefs, c(beta0, beta1, beta2))
+}
+
+colnames(coefs) <- c("beta0", "beta1", "beta2")
+
+plot(1:1000, coefs$beta0, col="red", type="l")
+lines(1:1000, coefs$beta1, col="green")
+lines(1:1000, coefs$beta2, col="blue")
+
+# f.
+lmfit <- lm(y~x1+x2, data=g)
+summary(lmfit)
+# the estimates are very close.
+abline(lmfit$coef[2],0, col="purple", lwd="2")
+abline(lmfit$coef[3],0, col="orange", lwd="2")
+abline(lmfit$coef[1],0, col="gray", lwd="2")
+# confirms that the fitted coefficients are extremely close.
+
+# g.
+# only about four iterations.
+
+# 12.
+h <- matrix(ncol=100, nrow=500)
+for(i in 1:100) {
+    h[,i] <- rnorm(500)
+}
+y <- rnorm(500)
+colnames(h) <- sprintf("x%s", 1:100)
+coefs <- data.frame()
+
+current.coefs <- rnorm(100)
+
+for(i in 1:10) {
+    for (i in 1:100) {
+        a <- y - h %*% current.coefs + h[,i] * current.coefs[i]
+        current.coefs[i] <- lm(a~h[,i])$coef[2]
+    }
+    coefs <- rbind(coefs, current.coefs)
+}
+colnames(coefs) <- sprintf("x%s", 1:100)
+
+plot(1:10, coefs$x1, type="l", col="blue", lwd=2, ylim=c(-.1,.1))
+lines(1:10, coefs$x2, type="l", col="red", lwd=2)
+lines(1:10, coefs$x3, type="l", col="green", lwd=2)
+lines(1:10, coefs$x4, type="l", col="purple", lwd=2)
+lines(1:10, coefs$x5, type="l", col="orange", lwd=2)
+
+# these all seem to stabilize around 4-5 iterations.
+# let's try an actual lm.
+
+h.f <- data.frame(h)
+h.f$y <- y
+summary(lm(y~., h.f))
+
+current.coefs[1]
+current.coefs[2]
+current.coefs[3]
+current.coefs[99]
+current.coefs[100]
+
+# we level out at the same values as the lm function.
